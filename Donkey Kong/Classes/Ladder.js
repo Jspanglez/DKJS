7d237e067d3497b62478edd7fd86e2abc9c4f930
Jeev Pangli <jspanglez@gmail.com>
2023-04-13 18:41:51 +0100

    Made if statements to allow for taller ladders

--- Donkey Kong/Classes/Ladder.js
+++ Donkey Kong/Classes/Ladder.js
@@ -12,7 +12,33 @@ export class Ladder {
     }
 
     drawLadder(ctx) {
-        ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y, this.w, this.w)
+        if (this.h == 1) {
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 15, 38, 38)
+        }
+        
+        else if (this.h == 2) {
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 15, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 30, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 33, 38, 38)
+        }
+
+        else if (this.h == 3) {
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 15, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 30, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 45, 38, 38)
+        }
+
+        else if (this.h == 4) {
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 15, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 30, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 45, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 60, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 75, 38, 38)
+            ctx.drawImage(img, 130, 151.5, 10, 9, this.x, this.y - 90, 38, 38)
     }
 
     /* drawLadder(ctx) {
