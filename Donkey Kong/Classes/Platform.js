f9a15e5ca8aed1de62f49b2bdde483a3a4b892ee
Jeev Pangli <jspanglez@gmail.com>
2023-04-13 18:39:06 +0100

    Removed unnecessary code

--- Donkey Kong/Classes/Platform.js
+++ Donkey Kong/Classes/Platform.js
@@ -1,14 +1,9 @@
 const scale = 3
 const width = 17
 const height = 22
-const scaledWidth = scale * width
-const scaledHeight = scale * height
 
-let img = new Image();
+let img = new Image()
 img.src = './mario_and_luigi_sprites.png'
-img.onload = function() {
-    new Platform()
-}
 
 export class Platform {
     constructor(x, y, w) {
@@ -24,19 +19,8 @@ export class Platform {
         const spriteY = 154
         
         for (let i = 0; i < this.w; i++) {
-          const x = this.x + i * 15
-          ctx.drawImage(img, spriteX, spriteY, 9, 8, x, this.y, spriteWidth, spriteHeight)
+            const x = this.x + i * 15
+            ctx.drawImage(img, spriteX, spriteY, 9, 8, x, this.y, spriteWidth, spriteHeight)
         }
     }
-      
-
-    /* drawPlatform(ctx) {
-        ctx.strokeStyle = "red"
-        ctx.beginPath()
-        ctx.lineWidth = 10
-        ctx.moveTo(this.x, this.y)
-        ctx.lineTo(this.x2, this.y2)
-        ctx.closePath()
-        ctx.stroke()
-    } */
 }
