31e11a56ba81c15406503d6eb6a1047ac3646c2a
Jeev Pangli <jspanglez@gmail.com>
2023-05-03 20:17:47 +0100

    Removed climbing method

--- Donkey Kong/Classes/sprites.js
+++ Donkey Kong/Classes/sprites.js
@@ -1,5 +1,5 @@
 let img = new Image()
-img.src = './mario_luigi_sprites.png'
+img.src = './mario_luigi_sprites2.png'
 
 let img2 = new Image()
 img2.src = './barrel_sprites.png'
@@ -7,27 +7,20 @@ img2.src = './barrel_sprites.png'
 let img3 = new Image()
 img3.src = './DK_sprites.png'
 
-let img4 = new Image()
-img4.src = './mario_luigi_climbing_sprites.png'
-
 export class sprites {
-	constructor(ctx) {
-		this.ctx = ctx
-	}
+    constructor(ctx) {
+        this.ctx = ctx
+    }
 
-	drawSprite(spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight) {
-		this.ctx.drawImage(img, spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight)
-	}
+    drawSprite(spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight) {
+        this.ctx.drawImage(img, spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight)
+    }
 
-	drawSpriteBarrel(spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight) {
-		this.ctx.drawImage(img2, spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight)
-	}
+    drawSpriteBarrel(spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight) {
+        this.ctx.drawImage(img2, spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight)
+    }
 
-	drawSpriteDK(spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight) {
-		this.ctx.drawImage(img3, spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight)
-	}
-
-	drawClimbing(spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight) {
-		this.ctx.drawImage(img4, spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight)
-	}
+    drawSpriteDK(spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight) {
+        this.ctx.drawImage(img3, spriteX, spriteY, spriteWidth, spriteHeight, canvasX, canvasY, canvasWidth, canvasHeight)
+    }
 }
